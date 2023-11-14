32336209cdafa685c432985cf169a34f4bc967c8
Joshua Omondi Ochieng <joshuaomondi3334#gmail.com>
2023-11-14 03:39:39 -0800

    new changes made

--- Frontend/angular/src/app/app-routing.module.ts
+++ Frontend/angular/src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    LandingPageComponent,
-    NavbarComponent
+
+  
+
   ],
   imports: [
     CommonModule
